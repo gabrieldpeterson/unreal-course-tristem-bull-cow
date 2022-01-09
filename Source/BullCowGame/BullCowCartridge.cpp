b70c6e567ed0a86377257a68443e265a0f6cce12
Gabriel Peterson <gabrieldpeterson@gmail.com>
2022-01-09 15:24:14 -0800

    Get basic functionality and user input working

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,23 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the four letter word.")); // TODO remove magic number
+    PrintLine(TEXT("Press enter to continue..."));
+
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You are correct"));
+    }
+    else
+    {
+        PrintLine(TEXT("Wrong"));
+    }
 }
